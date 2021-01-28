3cd628c5f4454bb2c90b8b9fffaa84184dba823c
mohhef <mohhef@rocketmail.com>
2021-01-28 03:39:58 -0500

    User ajax to submit forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search' #this is like https://xyz/searchstock that will be submitted to the stocks controller and search action
 end
